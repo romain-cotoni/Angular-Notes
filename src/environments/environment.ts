e009eca3f02443d7fcfa2eab765c40341ec416e2
romain-cotoni <romain.cotoni@gmail.com>
2023-12-31 00:09:26 +0100

    environment variable

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8080/api'
+    apiUrl: 'https://spring-notes.test.azuremicroservices.io/spring-notes/api' //'http://localhost:8080/api'
 };
