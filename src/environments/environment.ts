b450c8990fbbb9ad35cd26ba37678aaaf8c5c482
romain-cotoni <romain.cotoni@gmail.com>
2024-01-09 22:48:23 +0100

    configure vps url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://spring-notes-spring-notes.azuremicroservices.io/api' 
+    //apiUrl: 'https://spring-notes-spring-notes.azuremicroservices.io/api' 
     //apiUrl: 'http://localhost:8080/api'
+    apiUrl: 'http://vps-a1453f27.vps.ovh.net:8080/api'
 };
