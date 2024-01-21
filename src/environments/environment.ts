4e4cfafa15e256e8142edb375d0102ec467d7c03
romain-cotoni <romain.cotoni@gmail.com>
2024-01-21 21:06:57 +0100

    apiUrl variable set for vps spring local

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,6 @@ export const environment = {
     //apiUrl: 'https://spring-notes-spring-notes.azuremicroservices.io/api' 
     //apiUrl: 'http://localhost:8080/api'
     //apiUrl: 'http://vps-a1453f27.vps.ovh.net:8080/api'
-    apiUrl: 'https://notes-write.ovh:8080/api'
+    //apiUrl: 'https://notes-write.ovh:8080/api'
+    apiUrl: 'https://notes-write.ovh/api'
 };
