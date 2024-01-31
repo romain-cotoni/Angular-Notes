e2d6aaac679872618e4dc64f7aa84248b4c1c3cb
romain-cotoni <romain.cotoni@gmail.com>
2024-01-31 21:17:30 +0100

    apiUrl for vps

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
     production: false,
     //apiUrl: 'https://spring-notes-spring-notes.azuremicroservices.io/api' //azure 
     //apiUrl: 'http://vps-a1453f27.vps.ovh.net:8080/api'                    //vps
-    apiUrl: 'http://localhost:8080/api'                                  
-    //apiUrl: 'https://notes-write.ovh/api'                                 //vps + domaine name + https
+    //apiUrl: 'http://localhost:8080/api'                                  
+    apiUrl: 'https://notes-write.ovh/api'                                   //vps + domaine name + https
 };
